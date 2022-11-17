a3c602d644bb4fb7c2e5935a9c19a76eacab5d05
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2022-11-17 15:23:23 +0530

    Update adActions.js

--- FlipBottle/v1.0.3/adActions.js
+++ FlipBottle/v1.0.3/adActions.js
@@ -1,28 +1,24 @@
-canvas.addEventListener("click",function(e){
+var = document.querySelector('canvas');
+
+var context = cnvs.getContext('webgl2');
+	
+// where we'll store our pixels info
+var pixels = new Uint8Array(4);
+
+
+cnvs.addEventListener("click",function(e){
 	// Get the coordinates of the click
 	var pos = getPosition(this);
   	var x = e.pageX - pos.x;
   	var y = e.pageY - pos.y;
 	
 	// Get the data of the pixel according to the location generate by the getEventLocation function
-	var context = this.getContext("webgl2", {preserveDrawingBuffer: true});
-	read();
-	// where we'll store our pixels info
 	
-	
-	function read() {
-		var pixels = new Uint8Array(4);
+	requestAnimationFrame(function() {
+		
 		context.readPixels(x, y, 1, 1, context.RGBA, context.UNSIGNED_BYTE, pixels);
 		console.log(pixels);
-		}
-		
-    //var pixelData = context.getImageData(eventLocation.x, eventLocation.y, 1, 1).data; 
+		});
 
-    // If transparency on the pixel , array = [0,0,0,0]
-    //if((pixelData[0] == 0) && (pixelData[1] == 0) && (pixelData[2] == 0) && (pixelData[3] == 0)){
-        // Do something if the pixel is transparent
-    //}
-
-   //var hex = "#" + ("000000" + rgbToHex(pixelData[0], pixelData[1], pixelData[2])).slice(-6);
 		
 });
