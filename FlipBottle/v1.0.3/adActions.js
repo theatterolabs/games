4477c94ddb6450cba05df200db4927401787f189
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2022-11-17 16:18:23 +0530

    Update adActions.js

--- FlipBottle/v1.0.3/adActions.js
+++ FlipBottle/v1.0.3/adActions.js
@@ -1,22 +1,19 @@
-var cnvs = document.querySelector('canvas');
-	
-// where we'll store our pixels info
-var pixels = new Uint8Array(4);
-
+var target = document.querySelector('canvas');
+// the canvas we'll use to read the target on
+var reader = document.createElement('canvas');
+var ctx = reader.getContext('2d');
 
 cnvs.addEventListener("click",function(e){
 	// Get the coordinates of the click
-	var x = e.clientX - cnvs.offsetLeft;
-  	var y = e.clientY - cnvs.offsetTop;
-	var context = this.getContext("webgl2", {preserveDrawingBuffer: true});
-	
-	// Get the data of the pixel according to the location generate by the getEventLocation function
-	
-	
-		requestAnimationFrame(function() {
-		context.readPixels(x, y, 1, 1, context.RGBA, context.UNSIGNED_BYTE, pixels);
-		console.log(pixels);
-	
-		});
+	 var x = e.clientX - target.offsetLeft;
+  	var y = e.clientY - target.offsetTop;
+  // same preserveDrawingBuffer workaround
+  	requestAnimationFrame(function() {
+    // move the target image in the top left corner of our reader,
+    //  because we want only a single pixel
+   	 ctx.drawImage(target, -x, -y); 
+    	var pixels = ctx.getImageData(0, 0, 1, 1);
+    	console.log(pixels);
+  });
 		
 });
