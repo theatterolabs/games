ce2e3506f8993395f953f79082ae3d4cf97a34dc
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2022-11-17 15:56:57 +0530

    Update adActions.js

--- FlipBottle/v1.0.3/adActions.js
+++ FlipBottle/v1.0.3/adActions.js
@@ -12,11 +12,11 @@ cnvs.addEventListener("click",function(e){
 	
 	// Get the data of the pixel according to the location generate by the getEventLocation function
 	
-	requestAnimationFrame(function() {
+	
 		
 		context.readPixels(x, y, 1, 1, context.RGBA, context.UNSIGNED_BYTE, pixels);
 		console.log(pixels);
-		});
+	
 
 		
 });
