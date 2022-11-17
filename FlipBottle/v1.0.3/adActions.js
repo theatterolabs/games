8c826df28373eb6b1727f5404dc0dcaa8b0e8127
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2022-11-17 17:05:27 +0530

    Update adActions.js

--- FlipBottle/v1.0.3/adActions.js
+++ FlipBottle/v1.0.3/adActions.js
@@ -4,7 +4,7 @@ var pixels = new Uint8Array(4);
 
 target.addEventListener("click",function(event){
 	var eventLocation = getEventLocation(this,event);
-	var c = this.getContext("webgl2", {preserveDrawingBuffer: true});
+	var c = this.getContext("webgl2");
 	
 	requestAnimationFrame(function() {
 	c.readPixels(eventLocation.x, eventLocation.y, 1, 1, c.RGBA, c.UNSIGNED_BYTE, pixels);
