873b83dd7d67e088617ceecd1249e6f8b8d844fa
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2022-11-18 06:08:45 +0530

    Update adActions.js

--- FlipBottle/v1.0.3/adActions.js
+++ FlipBottle/v1.0.3/adActions.js
@@ -1,18 +1,18 @@
 var target = document.querySelector('canvas');
-// the canvas we'll use to read the target on
-var reader = document.createElement('canvas');
-var ctx = reader.getContext('2d');
 
-target.addEventListener("click",function(e){
-	//var eventLocation = getEventLocation(this,e);
-	 var x = e.clientX - target.offsetLeft;
- 	 var y = e.clientY - target.offsetTop;
-	
-	
-	requestAnimationFrame(function() {
-	 ctx.drawImage(target, -x, -y); 
-    	var pixels = ctx.getImageData(0, 0, 1, 1);
-    	console.log(pixels);
-		});
+target.prototype._getContext = target.prototype.getContext;
+
+target.addEventListener("click",function(){
+	target.prototype.getContext = function(contextType, contextAttributes) {
+    		console.log('INTERCEPTING: target.prototype.getContext');
+    		console.log('PROPERTIES:');
+    		console.log('  contextType: ' + contextType);
+
+    		return this._getContext(contextType, contextAttributes);
+	}
+		
 		
 });
+
+
+
