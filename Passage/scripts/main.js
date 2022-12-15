6be77a0a3c505b264441109eb30bb234695b3b9f
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2022-12-15 06:37:02 +0530

    Update main.js

--- Passage/scripts/main.js
+++ Passage/scripts/main.js
@@ -604,7 +604,7 @@
             else throw new Error(`unknown message '${type}'`);
         }
         _OnEventFromRuntime(e) {
-            console.log(e.data["originalUrl"]);
+            console.log(e.data);
             const component = e["component"];
             const handler = e["handler"];
             const data = e["data"];
