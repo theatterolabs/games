206981079d24b06d5863efa43180d3eaa50b4c41
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2023-01-30 04:11:49 +0530

    Create ads.js

---
+++ Knots/v1.4.1/scripts/ads.js
@@ -0,0 +1,46 @@
+var adsManager;
+var adsLoader;
+var adDisplayContainer;
+var playButton;
+var videoContent;
+var mainContainer = document.getElementById('mainContainer');
+var footerAD = document.getElementById('gamescreen-footer');
+var myTimer = setInterval(footerInterval, 20000);	
+
+
+function closeAds() {
+		myTimer = setInterval(footerInterval, 20000);	
+		mainContainer.src= 'about:blank';
+		footerAD.style.display = 'block';
+		mainContainer.style.display = 'none';
+		mixpanel.track('Skip Ad', {
+  			'Game Name': "Knots",
+			'Ad Media Type': "Interstitial"
+  		
+		});
+	
+}
+	
+	
+function closeEmptyInterstitialAds() {
+	footerAD.style.display = 'block';
+	myTimer = setInterval(footerInterval, 20000);
+	mainContainer.style.display = 'none';
+	mainContainer.src= 'about:blank';
+	
+}
+	
+
+	
+function requestAds() {
+	mainContainer.src= './interstitial.html';
+	footerAD.style.display = 'none';
+   	clearInterval(myTimer);
+	myTimer = 0;
+	mainContainer.style.display = 'block';
+	
+}
+		
+function footerInterval() {
+   refreshfooterSlot();
+ }
