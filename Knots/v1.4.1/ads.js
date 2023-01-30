66da95bb4360f05f83f4b16720560af764295a2a
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2023-01-30 13:31:30 +0530

    Delete ads.js

--- Knots/v1.4.1/ads.js
+++
@@ -1,8 +0,0 @@
-export default function requestAds() {
-	mainContainer.src= './interstitial.html';
-	footerAD.style.display = 'none';
-   	clearInterval(myTimer);
-	myTimer = 0;
-	mainContainer.style.display = 'block';
-	
-}
