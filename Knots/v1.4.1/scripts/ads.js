104a7111f1f80c683df90cb63a62428a93c7eba2
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2023-01-30 15:19:30 +0530

    Update ads.js

--- Knots/v1.4.1/scripts/ads.js
+++ Knots/v1.4.1/scripts/ads.js
@@ -1,4 +1,6 @@
-
+let mainContainer = document.getElementById('mainContainer');
+let footerAD = document.getElementById('gamescreen-footer');
+let myTimer = setInterval(footerInterval, 20000);
 
 export default function requestAds() {
 	
