ba3c68c7080e65015820f8283bbf2e63ba4ffc87
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2023-01-30 09:26:34 +0530

    Update ads.js

--- Knots/v1.4.1/ads.js
+++ Knots/v1.4.1/ads.js
@@ -32,7 +32,7 @@ function closeEmptyInterstitialAds() {
 	
 
 	
-function requestAds() {
+window.requestAds = function requestAds() {
 	mainContainer.src= './interstitial.html';
 	footerAD.style.display = 'none';
    	clearInterval(myTimer);
