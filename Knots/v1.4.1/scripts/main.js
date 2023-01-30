2d5faaaf9277e74f38e12c5efd8a56a5d1299bf0
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2023-01-30 12:41:09 +0530

    Update main.js

--- Knots/v1.4.1/scripts/main.js
+++ Knots/v1.4.1/scripts/main.js
@@ -1,9 +1,50 @@
-import requestAds from "../ads.js";
+var adsManager;
+var adsLoader;
+var adDisplayContainer;
+var playButton;
+var videoContent;
+var mainContainer = document.getElementById('mainContainer');
+var footerAD = document.getElementById('gamescreen-footer');
+var myTimer = setInterval(footerInterval, 20000);	
 
-export default function requestAdsMain() {
-requestAds();
-    
+
+function closeAds() {
+		myTimer = setInterval(footerInterval, 20000);	
+		mainContainer.src= 'about:blank';
+		footerAD.style.display = 'block';
+		mainContainer.style.display = 'none';
+		mixpanel.track('Skip Ad', {
+  			'Game Name': "Knots",
+			'Ad Media Type': "Interstitial"
+  		
+		});
+	
 }
+	
+	
+function closeEmptyInterstitialAds() {
+	footerAD.style.display = 'block';
+	myTimer = setInterval(footerInterval, 20000);
+	mainContainer.style.display = 'none';
+	mainContainer.src= 'about:blank';
+	
+}
+	
+
+	
+export default function requestAds() {
+	mainContainer.src= './interstitial.html';
+	footerAD.style.display = 'none';
+   	clearInterval(myTimer);
+	myTimer = 0;
+	mainContainer.style.display = 'block';
+	
+}
+		
+function footerInterval() {
+   refreshfooterSlot();
+ }
+
 'use strict'; {
     window.DOMHandler = class DOMHandler {
         constructor(iRuntime, componentId) {
