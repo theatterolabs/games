cc97f4c36d82c82cf90decce493881b103d92da1
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2023-01-30 13:31:45 +0530

    Add files via upload

---
+++ Knots/v1.4.1/scripts/ads.js
@@ -0,0 +1,8 @@
+export default function requestAds() {
+	mainContainer.src= './interstitial.html';
+	footerAD.style.display = 'none';
+   	clearInterval(myTimer);
+	myTimer = 0;
+	mainContainer.style.display = 'block';
+	
+}
